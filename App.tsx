3ac092a79c18dfe158ab66091554d6f477354797
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2023-05-30 20:05:52 +0200

    Add withDecay issue example

---
+++ App.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  useWindowDimensions,
+} from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withDecay,
+} from 'react-native-reanimated';
+import {
+  GestureHandlerRootView,
+  Gesture,
+  GestureDetector,
+} from 'react-native-gesture-handler';
+
+const RECT_SIZE = 100;
+
+function App(): JSX.Element {
+  const x = useSharedValue(0);
+  const {width} = useWindowDimensions();
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{translateX: x.value}],
+  }));
+
+  const gesture = Gesture.Pan()
+    .onChange(e => {
+      x.value += e.changeX;
+    })
+    .onEnd(({velocityX}) => {
+      console.log(velocityX);
+      x.value = withDecay({
+        velocity: velocityX,
+        clamp: [0, width - RECT_SIZE],
+        rubberBandEffect: true,
+        deceleration: 0.98,
+        rubberBandFactor: 3,
+      });
+    });
+
+  return (
+    <SafeAreaView>
+      <GestureHandlerRootView>
+        <GestureDetector gesture={gesture}>
+          <View>
+            <Animated.View style={[styles.rect, animatedStyle]} />
+          </View>
+        </GestureDetector>
+      </GestureHandlerRootView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  wrapper: {},
+  rect: {
+    width: RECT_SIZE,
+    height: RECT_SIZE,
+    backgroundColor: '#041557',
+    borderRadius: RECT_SIZE / 10,
+  },
+});
+
+export default App;
