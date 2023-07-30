0f8eea4dc4cc4683a69501ae90c72a7bafd00910
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2023-07-30 18:25:45 +0200

    Add simple issue example

--- App.tsx
+++ App.tsx
@@ -1,67 +1,49 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  View,
-  useWindowDimensions,
-} from 'react-native';
-import Animated, {
-  useAnimatedStyle,
-  useSharedValue,
-  withDecay,
-} from 'react-native-reanimated';
+import React, {useEffect} from 'react';
+import {StyleSheet, View} from 'react-native';
 import {
   GestureHandlerRootView,
   Gesture,
   GestureDetector,
 } from 'react-native-gesture-handler';
-
-const RECT_SIZE = 100;
+import {
+  useAnimatedReaction,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
 function App(): JSX.Element {
-  const x = useSharedValue(0);
-  const {width} = useWindowDimensions();
+  const dummyValue = useSharedValue(0);
 
-  const animatedStyle = useAnimatedStyle(() => ({
-    transform: [{translateX: x.value}],
-  }));
+  const gesture = Gesture.Pan();
 
-  const gesture = Gesture.Pan()
-    .onChange(e => {
-      x.value += e.changeX;
-    })
-    .onEnd(({velocityX}) => {
-      console.log(velocityX);
-      x.value = withDecay({
-        velocity: velocityX,
-        clamp: [0, width - RECT_SIZE],
-        rubberBandEffect: true,
-        deceleration: 0.98,
-        rubberBandFactor: 3,
-      });
-    });
+  useEffect(() => {
+    dummyValue.value = withTiming(1, {duration: 10000});
+  }, []);
+
+  useAnimatedReaction(
+    () => dummyValue.value,
+    v => {
+      console.log(v);
+    },
+  );
 
   return (
-    <SafeAreaView>
-      <GestureHandlerRootView>
-        <GestureDetector gesture={gesture}>
-          <View>
-            <Animated.View style={[styles.rect, animatedStyle]} />
-          </View>
-        </GestureDetector>
-      </GestureHandlerRootView>
-    </SafeAreaView>
+    <GestureHandlerRootView style={styles.root}>
+      <GestureDetector gesture={gesture}>
+        <View style={styles.view} />
+      </GestureDetector>
+    </GestureHandlerRootView>
   );
 }
+export default App;
 
 const styles = StyleSheet.create({
-  wrapper: {},
-  rect: {
-    width: RECT_SIZE,
-    height: RECT_SIZE,
-    backgroundColor: '#041557',
-    borderRadius: RECT_SIZE / 10,
+  root: {
+    flex: 1,
+  },
+  view: {
+    flex: 1,
+    margin: 20,
+    backgroundColor: '#06196D',
   },
 });
-
-export default App;
